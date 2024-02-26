7bc5aba4025d7afe60b8ebff258a1801d41ea542
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2024-02-26 17:45:22 +0100

    Add test case for DPoPProof::__toString

---
+++ tests/Model/DPoPProofTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace danielburger1337\OAuth2DPoP\Model;
+
+use danielburger1337\OAuth2DPoP\JwtHandler\JwkInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(DPoPProof::class)]
+class DPoPProofTest extends TestCase
+{
+    #[Test]
+    public function __toString_returnsProof(): void
+    {
+        $jwk = $this->createMock(JwkInterface::class);
+
+        $proof = new DPoPProof($jwk, 'abcdefg');
+
+        $returnValue = $proof->__toString();
+
+        $this->assertEquals('abcdefg', $returnValue);
+    }
+}
