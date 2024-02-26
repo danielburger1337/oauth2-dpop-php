7e33a8aecd29aa7e709655c1c78e6003c5622e80
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2024-02-26 17:49:29 +0100

    Add PHPDoc to Model\AccessTokenModel

--- src/Model/AccessTokenModel.php
+++ src/Model/AccessTokenModel.php
@@ -5,10 +5,13 @@ namespace danielburger1337\OAuth2DPoP\Model;
 final class AccessTokenModel
 {
     /**
+     * @param \Stringable|string $accessToken The access token that must be bound to a JKT.
+     * @param string             $jkt         The JKT the access token must be bound to.
+     *
      * @codeCoverageIgnore
      */
     public function __construct(
-        public readonly string|\Stringable $accessToken,
+        public readonly \Stringable|string $accessToken,
         public readonly string $jkt
     ) {
     }
