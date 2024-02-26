7c634d0186a651a7c138c1e554db131a567d92e6
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2024-02-26 14:34:37 +0100

    Mark WebTokenFrameworkJwk as internal

--- src/JwtHandler/WebTokenFrameworkJwk.php
+++ src/JwtHandler/WebTokenFrameworkJwk.php
@@ -5,7 +5,10 @@ namespace danielburger1337\OAuth2DPoP\JwtHandler;
 use Jose\Component\Core\Algorithm;
 use Jose\Component\Core\JWK;
 
-class WebTokenFrameworkJwk implements JwkInterface
+/**
+ * @internal
+ */
+final class WebTokenFrameworkJwk implements JwkInterface
 {
     public function __construct(
         public readonly JWK $jwk,
