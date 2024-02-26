545703422505d6fb0be2ca4ab0d6486f8a31c901
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2024-02-26 17:47:57 +0100

    Add PHPDoc to Model\DecodedDPoPProof

--- src/Model/DecodedDPoPProof.php
+++ src/Model/DecodedDPoPProof.php
@@ -5,8 +5,9 @@ namespace danielburger1337\OAuth2DPoP\Model;
 final class DecodedDPoPProof
 {
     /**
-     * @param array<string, mixed> $payload
-     * @param array<string, mixed> $protectedHeader
+     * @param string               $jwkThumbprint   The JKT of the JWK that signed the DPoP proof.
+     * @param array<string, mixed> $payload         The decoded payload of the DPoP proof token.
+     * @param array<string, mixed> $protectedHeader The decoded protected header of the DPoP proof token.
      *
      * @codeCoverageIgnore
      */
