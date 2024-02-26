c398f1606ea41904e780c6988a3d95b79e8bd941
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2024-02-26 13:48:19 +0100

    Add PHPDoc to NonceStorageInterface

--- src/NonceStorage/NonceStorageInterface.php
+++ src/NonceStorage/NonceStorageInterface.php
@@ -4,11 +4,37 @@ namespace danielburger1337\OAuth2DPoP\NonceStorage;
 
 interface NonceStorageInterface
 {
+    /**
+     * Create a new DPoP-Nonce.
+     *
+     * @param string $key The key under which to store the nonce.
+     *
+     * @return string The created nonce.
+     */
     public function createNewNonce(string $key): string;
 
+    /**
+     * Whether the DPoP-Nonce is valid.
+     *
+     * @param string $key   The storage key to use for comparisson.
+     * @param string $nonce The nonce to compare.
+     */
     public function isNonceValid(string $key, string $nonce): bool;
 
-    public function storeNextNonce(string $key, string $nonce): void;
-
+    /**
+     * Get the current DPoP-Nonce.
+     *
+     * @param string $key The storage key to use.
+     *
+     * @return string|null The current nonce or `null` if none exists.
+     */
     public function getCurrentNonce(string $key): ?string;
+
+    /**
+     * Store a new DPoP-Nonce.
+     *
+     * @param string $key   The storage key to use.
+     * @param string $nonce The nonce to store.
+     */
+    public function storeNextNonce(string $key, string $nonce): void;
 }
